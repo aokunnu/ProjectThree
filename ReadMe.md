d4233640f88648fe1608066f3dc476a4e2de8216
aokunnu <aokunnu5@gmail.com>
2020-09-18 14:43:23 -0500

    First Commit

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+# ProjectThree
+# Marketing Data
+
+Howdy All! You access this repository in order to downlaod analytical marketing data regarding HTTP server logs from the following web server: https://s3.amazonaws.com/tcmg476/http_access_log
+
+In order to download the data perfom the following:
+1. Clone this repository onto a Linux OS (I used Ubuntu 20.04.1 LTS)
+2. Run the file 'read_log.py' by using the following command:
+$ python3 read_log.py
+3. Run it and enjoy!
+
+Enjoy!
---
+++ read_log.py
@@ -0,0 +1,18 @@
+FILE_NAME = './local_copy.log'
+
+from collections import Counter
+
+last = 0
+total = 0
+
+for line in open(FILE_NAME):
+      repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
+        year = repl.split('#')
+          total += 1
+            if len(year)>=3:
+                    if year[3] == str(1995):
+                                last += 1
+
+                                print("There were ", last, "total requests made in the last year.")
+                                print("There were ", total, "total requests made in the time period represented by the log.")
+                                
