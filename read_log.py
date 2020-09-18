6cd5707637dfe75cba3aa07ce45ded84a9a31693
aokunnu <aokunnu5@gmail.com>
2020-09-18 15:30:30 -0500

    fixed .py files

--- read_log.py
+++ read_log.py
@@ -6,13 +6,13 @@ last = 0
 total = 0
 
 for line in open(FILE_NAME):
-      repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
-        year = repl.split('#')
-          total += 1
-            if len(year)>=3:
-                    if year[3] == str(1995):
-                                last += 1
+    repl = line.replace("[", "#", 10).replace(":", "#", 10).replace("/", "#", 10)
+    year = repl.split('#')
+    total += 1
+    if len(year)>=3:
+        if year[3] == str(1995):
+             last += 1
 
-                                print("There were ", last, "total requests made in the last year.")
-                                print("There were ", total, "total requests made in the time period represented by the log.")
+print("There were ", last, "total requests made in the last year.")
+print("There were ", total, "total requests made in the time period represented by the log.")
                                 
